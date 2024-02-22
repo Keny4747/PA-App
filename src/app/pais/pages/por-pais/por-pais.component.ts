d1943936c07f2a168983105fd5206ae3e4fb7d08
Keny Pau Vas <keny_pauccarima@usmp.pe>
2024-02-22 16:33:29 -0500

    ...

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styleUrls: ['./por-pais.component.css']
+})
+export class PorPaisComponent {
+
+  termino:string = '';
+  constructor(){}
+
+
+  buscar(){
+    console.log(this.termino);
+  }
+}
